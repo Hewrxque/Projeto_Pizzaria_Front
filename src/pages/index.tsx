52d90eb201604d8e8c324a366f83f131bee06644
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-08-17 19:37:20 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import Head from "next/head";
-import logo from '../../public/Pizzaria.png'
+import logo from "../../public/Pizzaria.png";
+
 import Image from "next/image";
-import styles from '../../styles/home.module.scss'
+
+import styles from "../../styles/home.module.scss";
+import { Input } from "../components/ui/input";
+
 export default function Home() {
   return (
     <>
@@ -9,7 +13,15 @@ export default function Home() {
         <title>Rick's Pizza - Faça seu login</title>
       </Head>
       <div className={styles.containerCenter}>
-      <Image src={logo} alt="Rick's Pizza"/>
+        <Image src={logo} alt="Rick's Pizza" />
+
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Digite seu email" type="text"/>
+
+            <Input placeholder="Digite sua senha" type="password"/>
+          </form>
+        </div>
       </div>
     </>
   );
