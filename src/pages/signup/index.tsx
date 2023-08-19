8ff479a4944313774032e4007acca3a76510069b
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-08-19 00:17:36 -0300

    create signUp

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,25 +1,29 @@
 import Head from "next/head";
-import logo from "../../public/Pizzaria.png";
+import logo from "../../../public/Pizzaria.png";
 
 import Image from "next/image";
 
-import styles from "../../styles/home.module.scss";
-import { Input } from "../components/ui/input";
-import { Button } from "../components/ui/button";
+import styles from "../../../styles/home.module.scss";
+import { Input } from "../../components/ui/input";
+import { Button } from "../../components/ui/button";
 
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>Rick's Pizza - Faça seu login</title>
+        <title>Faça seu cadastro agora!</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image className={styles.image} src={logo} alt="Rick's Pizza" />
 
         <div className={styles.login}>
+          <h1>Criando sua conta</h1>
+
           <form>
+            <Input placeholder="Digite seu nome" type="text" />
+
             <Input placeholder="Digite seu email" type="text" />
 
             <Input placeholder="Digite sua senha" type="password" />
@@ -29,10 +33,9 @@ export default function Home() {
             </Button>
           </form>
 
-         
-            <a href="/signup" className={styles.text}>Nao possui uma conta? Cadastre-se!</a>
-         
-
+          <a href="/" className={styles.text}>
+            Já possui uma conta? Faça seu login!
+          </a>
         </div>
       </div>
     </>
