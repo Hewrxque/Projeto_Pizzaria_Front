ea3d9aac5f846e841eac58220138891b59648659
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-08-19 21:01:59 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useContext, FormEvent } from "react";
 import Head from "next/head";
 import logo from "../../public/Pizzaria.png";
 
@@ -7,9 +8,24 @@ import styles from "../../styles/home.module.scss";
 import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";
 
+import { AuthContext } from "../contexts/AuthContext";
+
 import Link from "next/link";
 
 export default function Home() {
+  const {signIn } = useContext(AuthContext)
+
+  async function handleLogin(event: FormEvent){
+    event.preventDefault();
+
+    let data = {
+      email: "algum@teste.com",
+      password: "123456"
+    }
+    
+    await signIn(data)
+  }
+
   return (
     <>
       <Head>
@@ -19,7 +35,7 @@ export default function Home() {
         <Image className={styles.image} src={logo} alt="Rick's Pizza" />
 
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input placeholder="Digite seu email" type="text" />
 
             <Input placeholder="Digite sua senha" type="password" />
