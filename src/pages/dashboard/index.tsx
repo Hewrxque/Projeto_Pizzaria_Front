6bdbe5a905f643817107e71651736b9216b547c0
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-08-21 23:22:22 -0300

    update

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import { canSSRAuth } from "../../utils/canSSRAuth";
+import styles from "./styles.module.scss";
 
 import { Header } from "../../components/Header";
-
+import { FiRefreshCcw } from 'react-icons/fi'
 export default function Dashboard() {
   return (
     <>
@@ -14,9 +15,27 @@ export default function Dashboard() {
 
      <div>
       <Header/>
-      <h1>
-        Painel
-      </h1>
+
+    <main className={styles.container}>
+
+      <div className={styles.containerHeader}>
+        <h1>Ultimos pedidos</h1>
+        <button>
+          <FiRefreshCcw size={40} color="#3FFFA3"/>
+        </button>
+      </div>
+
+      <article className={styles.listOrders}>
+        <section className={styles.orderItem}>
+          <button>
+            <div className={styles.tag}></div>
+            <span> Mesa 30</span>
+          </button>
+        </section>
+
+      </article>
+
+    </main>
      </div>
     </>
   );
